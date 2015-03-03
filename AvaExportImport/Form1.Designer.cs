36699480459e303811cb85918cc242af153df30a
Lars White <lars@lars-san.com>
2015-03-03 14:57:35 -0800

    Massive update
    At some point I started working in a different copy of the project,
    making this a massive update from the previous version.

---
+++ AvaExportImport/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace AvaTaxImport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.FilePath = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Process Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Company Code";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Input File";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(129, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(129, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "[Blank]";
+            // 
+            // FilePath
+            // 
+            this.FilePath.AutoSize = true;
+            this.FilePath.Location = new System.Drawing.Point(129, 62);
+            this.FilePath.Name = "FilePath";
+            this.FilePath.Size = new System.Drawing.Size(132, 17);
+            this.FilePath.TabIndex = 5;
+            this.FilePath.Text = "C:\\working directory";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(947, 28);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.saveAsToolStripMenuItem.Text = "Save As...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 108);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(920, 288);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(318, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 408);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.FilePath);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "AvaTax Import Tool";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label FilePath;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AvaExportImport/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+
+namespace AvaTaxImport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Shows the openFileDialog
+            openFileDialog1.ShowDialog();
+            //Reads the text file
+            System.IO.StreamReader OpenFile = new System.IO.StreamReader(openFileDialog1.FileName);
+            //Displays the text file in the textBox
+            textBox1.Text = OpenFile.ReadToEnd();
+            //Stores the full path for the file and file name for later use
+            FilePath.Text = openFileDialog1.FileName.ToString();
+            //Closes the process
+            OpenFile.Close();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = "Ava_Import";
+            sfd.Filter = "CSV (*.csv)|*.csv";
+            //Open the saveFileDialog
+            sfd.ShowDialog();
+            //Determines the text file to save to
+            if (sfd.FileName != null)
+            {
+                System.IO.StreamWriter SaveFile = new System.IO.StreamWriter(sfd.FileName);
+                //Writes the text to the file
+                SaveFile.WriteLine(textBox1.Text);
+                //Closes the process
+                SaveFile.Close();
+            }
+        }
+        //----------------------------------------------------------------
+        private string[] FormatSwitch()
+        {
+            DoesMapExist();
+            string[] SwitchTable = new string[76];
+            TextFieldParser parser = new TextFieldParser("mapping.ini");
+            parser.SetDelimiters(",");
+            string[] fields;
+            int row = 0;
+            while (!parser.EndOfData)
+            {
+                fields = parser.ReadFields();
+                foreach (string field in fields)
+                {
+                    SwitchTable[row] = field;
+                    if(field == null)
+                    { MessageBox.Show(field); }
+                }
+                row = row + 1;
+            }
+            return SwitchTable;
+        }
+
+        private void DoesMapExist()
+        {
+            if (!File.Exists("mapping.ini"))
+            {
+                // This is a hard-coded version of mapping to go from the Export Document Lines report format to the import format.
+                string[] SwitchTable = new string[76];
+                SwitchTable[0] = "0"; // Process Code
+                SwitchTable[1] = "4"; // Company Code
+                SwitchTable[2] = "0";
+                SwitchTable[3] = "1"; // DocCode
+                SwitchTable[4] = "2"; // DocType
+                SwitchTable[5] = "3"; // ?
+                SwitchTable[6] = "9"; // TaxDate
+                SwitchTable[7] = "0"; // N/A
+                SwitchTable[8] = "0"; // N/A
+                SwitchTable[9] = "0"; // N/A
+                SwitchTable[10] = "0"; // N/A
+                SwitchTable[11] = "32"; // ?
+                SwitchTable[12] = "5"; // ?
+                SwitchTable[13] = "6"; // ?
+                SwitchTable[14] = "17"; // ?
+                SwitchTable[15] = "31"; // ?
+                SwitchTable[16] = "0"; // N/A
+                SwitchTable[17] = "30"; // ?
+                SwitchTable[18] = "29"; // ?
+                SwitchTable[19] = "7"; // ?
+                SwitchTable[20] = "10"; // ?
+                SwitchTable[21] = "11"; // ?
+                SwitchTable[22] = "8"; // ?
+                SwitchTable[23] = "0"; // N/A
+                SwitchTable[24] = "0"; // N/A
+                SwitchTable[25] = "12"; // ?
+                SwitchTable[26] = "13"; // Line Amount
+                SwitchTable[27] = "14"; // ?
+                SwitchTable[28] = "15"; // ?
+                SwitchTable[29] = "16"; // ?
+                SwitchTable[30] = "18"; // ?
+                SwitchTable[31] = "0"; // N/A
+                SwitchTable[32] = "0"; // N/A
+                SwitchTable[33] = "0"; // N/A
+                SwitchTable[34] = "41"; // Tax Amount & Tax Includes
+                SwitchTable[35] = "0"; // N/A
+                SwitchTable[36] = "0"; // N/A
+                SwitchTable[37] = "0"; // N/A
+                SwitchTable[38] = "0"; // N/A
+                SwitchTable[39] = "24"; // ?
+                SwitchTable[40] = "25"; // ?
+                SwitchTable[41] = "26"; // ?
+                SwitchTable[42] = "28"; // ?
+                SwitchTable[43] = "27"; // ?
+                SwitchTable[44] = "0"; // N/A
+                SwitchTable[45] = "19"; // ?
+                SwitchTable[46] = "20"; // ?
+                SwitchTable[47] = "21"; // ?
+                SwitchTable[48] = "23"; // ?
+                SwitchTable[49] = "22"; // ?
+                Write_ini("mapping.ini", SwitchTable);
+            }
+        }
+
+        private string[] ReadForImportant(string[] mapping) // This function is used to detect the fields that must be handled separately later.
+        {
+            string[] HandleSep = new string[6];
+            int row = 0;
+            while (row < 76)
+            {
+                if (mapping[row] == "0") // Detects for the Process Code
+                { HandleSep[0] = row.ToString(); }
+                else if (mapping[row] == "2") // Detects for the DocType
+                { HandleSep[1] = row.ToString(); }
+                else if (mapping[row] == "4") // Detects for the Company Code
+                { HandleSep[2] = row.ToString(); }
+                else if (mapping[row] == "9") // Detects for the Tax Date, needed for creating inverse
+                { HandleSep[3] = row.ToString(); }
+                else if (mapping[row] == "13") // Detects for Line Amount
+                { HandleSep[4] = row.ToString(); }
+                else if (mapping[row] == "41") // Detects for Tax Amount
+                { HandleSep[5] = row.ToString(); }
+                row = row + 1;
+            }
+            return HandleSep;
+        }
+
+        private void ConvertExport(string ProcessCode, string CompanyCode, bool taxincludes)
+        {
+            //int lineCount = textBox1.Lines.Count();
+            //lineCount -= String.IsNullOrWhiteSpace(textBox1.Text) ? 1 : 0;
+            int lineCount = File.ReadAllLines(FilePath.Text).Length;
+            //Console.WriteLine("Processing " + lineCount + " lines."); - This needs to be replaced with the Windows Form version
+            string[,] data = new string[lineCount, 76];
+            TextFieldParser parser = new TextFieldParser(FilePath.Text); // File name required in quotes
+
+            parser.HasFieldsEnclosedInQuotes = true;
+            parser.SetDelimiters(",");
+            string[] fields;
+            int row = 0;
+            int column = 0;
+            bool taxdateoverride = false;
+            double line_amount;
+            double tax_amount;
+            string ti_string;
+            string[] ST = new string[76];
+            ST = FormatSwitch();
+            string[] HandleSep = new string[6];
+            HandleSep = ReadForImportant(ST);
+            while (!parser.EndOfData)
+            {
+                fields = parser.ReadFields();
+                foreach (string field in fields)
+                {
+                    data[0, 0] = field; // testing
+                    if (Convert.ToInt32(ST[column]) != 0) // We must skip over column 0 and the undifined columns
+                    { data[row, Convert.ToInt32(ST[column])] = "\"" + field + "\""; }// This uses the previously defined mapping to reorgainize the data in the "data" array.
+                    if(column == Convert.ToInt32(HandleSep[0]))
+                    {
+                        data[row, 0] = ProcessCode;
+                    }
+                    else if(column == Convert.ToInt32(HandleSep[1]))
+                    {
+                        //data[row, 2] = "0"; //This is a temporary change, which should be rolled into an enhancement
+                        if (field == "Sales Invoice")
+                        { data[row, 2] = "1"; }
+                        else if (field == "Return Invoice")
+                        {
+                            data[row, 2] = "5";
+                            taxdateoverride = true;
+                        }
+                        else { data[row, 2] = "1"; } // This assumes that if the data isn't "Sales Invoice" or "Return Invoice", that the intention was for this to be an invoice
+                    }
+                    else if(column == Convert.ToInt32(HandleSep[2]))
+                    {
+                        data[row, 4] = "\"" + CompanyCode + "\"";
+                    }
+                    else if(column == Convert.ToInt32(HandleSep[3]))
+                    {
+                        if (taxdateoverride)
+                            {
+                                data[row, 9] = field; // This will set the date override date to the date used in the original, but only for return invoices.
+                                taxdateoverride = false;
+                            }
+                    }
+                    else if(column == Convert.ToInt32(HandleSep[5]))
+                    {
+                        if (taxincludes && row != 0) // This looks for the tax includes flag, which can be set by the app user, and it skips the header row.
+                            {
+                                tax_amount = Convert.ToDouble(field);
+                                line_amount = Convert.ToDouble(data[row, 13]);
+                                line_amount = line_amount + tax_amount;
+                                ti_string = Convert.ToString(line_amount);
+                                data[row, 13] = ti_string; // This makes the line amount equal to the original amount plus the tax
+                                data[row, 36] = "1"; // This sets the Tax Includes field to True on the import
+                            }
+                    }
+                    column = column + 1;
+                }
+                column = 0;
+                row = row + 1;
+            }
+            parser.Close();
+            // Writes the default import file header to row 0
+            data = RedoDefaultHeader(data);
+            textBox1.Text = UpdateFilePreview(data);
+        }
+
+        private string[,] RedoDefaultHeader(string[,] data)
+        {
+            data[0, 0] = "ProcessCode"; data[0, 1] = "DocCode"; data[0, 2] = "DocType"; data[0, 3] = "DocDate"; data[0, 4] = "CompanyCode"; data[0, 5] = "CustomerCode"; data[0, 6] = "EntityUseCode"; data[0, 7] = "LineNo"; data[0, 8] = "TaxCode"; data[0, 9] = "TaxDate"; data[0, 10] = "ItemCode"; data[0, 11] = "Description"; data[0, 12] = "Qty"; data[0, 13] = "Amount"; data[0, 14] = "Discount"; data[0, 15] = "Ref1"; data[0, 16] = "Ref2"; data[0, 17] = "ExemptionNo"; data[0, 18] = "RevAcct"; data[0, 19] = "DestAddress"; data[0, 20] = "DestCity"; data[0, 21] = "DestRegion"; data[0, 22] = "DestPostalCode"; data[0, 23] = "DestCountry"; data[0, 24] = "OrigAddress"; data[0, 25] = "OrigCity"; data[0, 26] = "OrigRegion"; data[0, 27] = "OrigPostalCode"; data[0, 28] = "OrigCountry"; data[0, 29] = "LocationCode"; data[0, 30] = "SalesPersonCode"; data[0, 31] = "PurchaseOrderNo"; data[0, 32] = "CurrencyCode"; data[0, 33] = "ExchangeRate"; data[0, 34] = "ExchangeRateEffDate"; data[0, 35] = "PaymentDate"; data[0, 36] = "TaxIncluded"; data[0, 37] = "DestTaxRegion"; data[0, 38] = "OrigTaxRegion"; data[0, 39] = "Taxable"; data[0, 40] = "TaxType"; data[0, 41] = "TotalTax"; data[0, 42] = "CountryName"; data[0, 43] = "CountryCode"; data[0, 44] = "CountryRate"; data[0, 45] = "CountryTax"; data[0, 46] = "StateName"; data[0, 47] = "StateCode"; data[0, 48] = "StateRate"; data[0, 49] = "StateTax"; data[0, 50] = "CountyName"; data[0, 51] = "CountyCode"; data[0, 52] = "CountyRate"; data[0, 53] = "CountyTax"; data[0, 54] = "CityName"; data[0, 55] = "CityCode"; data[0, 56] = "CityRate"; data[0, 57] = "CityTax"; data[0, 58] = "Other1Name"; data[0, 59] = "Other1Code"; data[0, 60] = "Other1Rate"; data[0, 61] = "Other1Tax"; data[0, 62] = "Other2Name"; data[0, 63] = "Other2Code"; data[0, 64] = "Other2Rate"; data[0, 65] = "Other2Tax"; data[0, 66] = "Other3Name"; data[0, 67] = "Other3Code"; data[0, 68] = "Other3Rate"; data[0, 69] = "Other3Tax"; data[0, 70] = "Other4Name"; data[0, 71] = "Other4Code"; data[0, 72] = "Other4Rate"; data[0, 73] = "Other4Tax"; data[0, 74] = "ReferenceCode"; data[0, 75] = "BuyersVATNo";
+            return data;
+        }
+
+        private string UpdateFilePreview(string[,] data)
+        {
+            int column = 0;
+            string ConvertedOutput = "";
+            column = 0;
+            foreach (string field in data)
+            {
+                if (column <= 74)
+                {
+                    ConvertedOutput = ConvertedOutput + field + ",";
+                    column = column + 1;
+                    //break;
+                }
+                else
+                {
+                    ConvertedOutput = ConvertedOutput + "\n";
+                    column = 0;
+                    //break;
+                }
+            }
+            return ConvertedOutput;
+        }
+
+        static void Write_ini(string filename, string[] data)
+        {
+            System.IO.File.WriteAllText(filename, string.Empty); // Before writing to the file, this empties the file. This way if there were previous contents with more lines than we are writing now, we will not have any of the old contents.
+            try
+            {
+                var fs = File.Open(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                var sw = new StreamWriter(fs);
+                foreach (string field in data)
+                {
+                    sw.WriteLine(field);
+                }
+                sw.Flush();
+                fs.Close();
+            }
+            catch (Exception e)
+            {
+                // Console.WriteLine("Exception: " + e.Message); // Replace with Windows Form messaging
+            }
+            // Console.WriteLine("Done.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConvertExport("4", "", false);
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            
+        }
+        //----------------------------------------------------------------
+    }
+}
